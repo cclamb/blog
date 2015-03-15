c832d302a7d4aa6b687f0a7f2071b46699c48a86
cclamb <chrislambistan@gmail.com>
2015-03-15 09:09:17 -0600

    added author migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313121021) do
+ActiveRecord::Schema.define(version: 20150315150422) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "author"
   end
 
 end
